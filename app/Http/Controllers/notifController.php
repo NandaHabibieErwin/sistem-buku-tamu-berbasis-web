089889e83255287232bdba2770b8a6a0e27b9bd5
Blackjack Colver Voltaire <qerasawe1@gmail.com>
2024-01-10 11:38:47 +0700

    Finished the System

--- app/Http/Controllers/notifController.php
+++ app/Http/Controllers/notifController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\notifModel;
 use Illuminate\Support\Facades\Auth;
-
+//
 class notifController extends Controller
 {
     public function markAsRead()
